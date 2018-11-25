9b43b8f06a9f865017c4ab32d93517770898176a
Derrick Persson <derrickpersson@gmail.com>
2018-11-24 16:36:06 -0800

    user-registration: adding validation

--- imports/ui/register.js
+++ imports/ui/register.js
@@ -74,22 +74,59 @@
 // jQuery(function(){ BasePagesRegister.init(); });
 
 import { Template } from "meteor/templating";
+import { Meteor } from "meteor/meteor";
 
 import "./register.html";
 
-Template.register.events({
-    'submit form'(event) {
-        event.preventDefault();
-        const username = event.target["register-username"].value;
-        const email = event.target["register-email"].value;
-        const password = event.target["register-password"].value;
-        const password2 = event.target["register-password2"].value;
-        const terms = event.target["register-terms"].value;
-        Accounts.createUser({
-            username,
-            email,
-            password,
-            terms,
-        });
-    }
-})
+// if (Meteor.isClient) {
+    Template.register.events({
+        'submit form'(event) {
+            event.preventDefault();
+            const username = event.target["register-username"].value;
+            const email = event.target["register-email"].value;
+            const password = event.target["register-password"].value;
+            const password2 = event.target["register-password2"].value;
+            const terms = event.target["register-terms"].value;
+            Accounts.createUser({
+                username,
+                email,
+                password,
+                password2,
+                terms,
+            });
+        }
+    })
+// }
+
+// if (Meteor.isServer) {
+//     console.log("Does this run?");
+//     // Accounts.validateNewUser((user) => {
+//     //     debugger
+//     //     console.log("It ran!");
+//     //     if(user.username && user.username.length >= 3) {
+//     //         return true;
+//     //     } else {
+//     //         throw new Meteor.Error(403, 'Your username must consist of at least 3 characters');
+//     //     }
+//     // });
+
+//     // Accounts.validateNewUser((user) => {
+//     //     if (user.password && user.password >= 5){
+//     //         return true;
+//     //     } else {
+//     //         throw new Meteor.Error(403, 'Your password must be at least 5 characters long');
+//     //     }
+//     // });
+
+//     // Accounts.validateNewUser((user) => {
+//     //     if(user.password2 && user.password2 >= 5) {
+//     //         if(user.password2 === user.password){
+//     //             return true;
+//     //         } else {
+//     //             throw new Meteor.Error(403, 'Please enter the same password as above');
+//     //         }
+//     //     } else {
+//     //         throw new Meteor.Error(403, 'Your password must be at least 5 characters long');
+//     //     }
+//     // });
+// }
